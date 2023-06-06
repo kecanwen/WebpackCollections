4b03dc657a7e2ee77a6e4510f3e69cb0fbf87f22
kecanwen <kcw199611252815@163.com>
2023-06-06 19:29:38 +0800

    css-loader style-loader

--- demo04/webpack.config.js
+++ demo04/webpack.config.js
@@ -3,6 +3,10 @@ module.exports = {
   output: {
     filename: 'bundle.js'
   },
+  /**
+   * css-loader 会对 @import 和 url() 进行处理，就像 js 解析 import/require() 一样。
+   * style-loader 把 CSS 插入到 DOM 中。
+   * */
   module: {
     rules:[
       {
