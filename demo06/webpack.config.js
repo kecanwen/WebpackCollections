faf179be6f091d207c5aec43460cba50b0965df1
kecanwen <kcw199611252815@163.com>
2023-06-07 10:46:41 +0800

    css-loader module

--- demo06/webpack.config.js
+++ demo06/webpack.config.js
@@ -24,6 +24,14 @@ module.exports = {
           {
              loader: 'css-loader',
              options: {
+               /**
+                * css-loader中的 modules: true 是用来启用 CSS Modules 功能的。
+                * 启用 CSS Modules 后，CSS 样式的规则会被打包进一个随机生成的哈希值的 class 名，以避免样式冲突。
+                * 这个 class 名字是由 CSS 内部名称和一个随机字符串组成的，保证了 class 名的独特性。
+                * 一般在 React 项目中使用较多。启用 CSS Modules 后，
+                * 你可以通过 import styles from './foo.module.css' 导入 CSS 文件，
+                * 然后通过 className={styles.example} 来使用该 CSS 样式
+                * */
                modules: true
              }
           }
