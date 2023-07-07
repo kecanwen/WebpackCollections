ed36e07028283d27106ba4196e24228c35d708d8
kecanwen <kcw199611252815@163.com>
2023-07-07 23:49:26 +0800

    升级到react@"^16.14.0" react-dom@16.14.0

---
+++ loader/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
+import './app.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>qwudqewrwqerhwhrwh</h1>
+      </div>
+    );
+  }
+};
+
+render((
+  <BrowserRouter>
+    <Route path="/" component={App} />
+  </BrowserRouter>
+), document.querySelector('#app'));
---
+++ loader/webpack.config.js
@@ -0,0 +1,31 @@
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'k_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      },
+    ]
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Webpack-demos',
+      filename: 'index.html'
+    }),
+  ]
+};
